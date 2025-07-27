a57d62ad1786fa7b28b63673f7096bbdeaa8325a
Ian <vanzyli101@gmail.com>
2025-07-27 14:04:10 +0200

    init

--- apps/backend/src/integrations/swagger/index.ts
+++ apps/backend/src/integrations/swagger/index.ts
@@ -15,7 +15,7 @@ export const initializeSwagger = () =>
             description: 'API documentation',
           },
         },
-        apis: ['./src/integrations/express/routers/**/*.ts'],
+        apis: ['./src/integrations/express/routers/**/*.ts', './apps/backend/src/integrations/express/routers/**/*.ts'],
       };
 
       swaggerSpec = swaggerJSDoc(swaggerOptions);
