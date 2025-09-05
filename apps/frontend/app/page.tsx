3de566373b2264f2abdf9e7f8df2592cf9f51aaf
Ian <vanzyli101@gmail.com>
2025-09-05 22:11:38 +0200

    Frontend

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,9 +1,11 @@
 import {testfunc} from "@nextjs-expressjs-postgresql/shared";
+import LoginComponent from "./auth/login/_components/login";
 
 export default function Home() {
   return (
     <div>
         {testfunc()}
+        <LoginComponent/>
     </div>
   );
 }
